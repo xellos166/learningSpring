1f24e76bc6d59a8ec1dedecd5165854e5904df7f
Xellos@166 <wdibangshu@gmail.com>
2022-06-18 11:34:37 +0530

    new classes added

---
+++ concept-spring-boot/src/main/java/com/ardent/spring/boot/config/MessageConverterConfig.java
@@ -0,0 +1,21 @@
+package com.ardent.spring.boot.config;
+
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+
+//@Configuration
+
+public class MessageConverterConfig {
+
+	// @Bean
+	public HttpMessageConverter<Object> createXmlHttpMessageConverter() {
+		MarshallingHttpMessageConverter xmlConverter = new MarshallingHttpMessageConverter();
+
+		XStreamMarshaller xstreamMarshaller = new XStreamMarshaller();
+		xmlConverter.setMarshaller(xstreamMarshaller);
+		xmlConverter.setUnmarshaller(xstreamMarshaller);
+
+		return xmlConverter;
+	}
+}
